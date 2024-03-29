148abca62d5550e2adbb56529ac8e1063b353e16
Nebiyyeh Dagdeviren <nebiyyehdagdeviren@gmail.com>
2024-03-29 01:48:34 -0500

    last arrangements

--- script.js
+++ script.js
@@ -9,10 +9,9 @@ async function fetchData(url) {
 }
 
 async function renderData(url) {
-  
   const container = document.querySelector(".coffees");
 
-  container.innerHTML='';
+  container.innerHTML = "";
 
   const data = await fetchData(url);
 
@@ -23,6 +22,18 @@ async function renderData(url) {
   data.forEach((item) => {
     const card = document.createElement("div");
     card.classList.add("card");
+
+    container.appendChild(card);
+
+    const text = document.createElement("div");
+    text.classList.add("text");
+
+    card.appendChild(text);
+
+    const pic = document.createElement("div");
+    pic.classList.add("pic");
+
+    card.appendChild(pic);
 
     const title = document.createElement("h3");
     title.textContent = item.title;
@@ -37,18 +48,18 @@ async function renderData(url) {
     image.src = item.image;
     image.alt = item.image;
 
-    card.appendChild(title);
-    card.appendChild(description);
-    card.appendChild(ingredients);
-    card.appendChild(image);
-    container.appendChild(card);
+    text.appendChild(title);
+    text.appendChild(description);
+    text.appendChild(ingredients);
+
+    pic.appendChild(image);
   });
 }
 
-document.getElementById('hot').addEventListener('click', () => {
-    renderData('https://api.sampleapis.com/coffee/hot');
+document.getElementById("hot").addEventListener("click", () => {
+  renderData("https://api.sampleapis.com/coffee/hot");
 });
 
-document.getElementById('iced').addEventListener('click', () => {
-    renderData('https://api.sampleapis.com/coffee/iced');
+document.getElementById("iced").addEventListener("click", () => {
+  renderData("https://api.sampleapis.com/coffee/iced");
 });
